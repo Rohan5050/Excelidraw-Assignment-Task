852d4028ff8d201e8cc494cd81394fa75eedd450
Rohan Vohra <rohanvohra055@gmail.com>
2025-03-19 23:26:45 +0530

    Updated page.tsx

--- apps/excelidraw-frontend/app/canvas/[roomId]/page.tsx
+++ apps/excelidraw-frontend/app/canvas/[roomId]/page.tsx
@@ -1,23 +1,23 @@
-import { RoomCanvas } from "@/components/RoomCanvas";
-interface PageProps {
-    params: { roomId: string };
-}
-  
-  export default function CanvasPage({ params }: PageProps) {
-    return <RoomCanvas roomId={params.roomId} />;
-  }
-  
-
-
 /*import { RoomCanvas } from "@/components/RoomCanvas";
 
-type PageProps = {
-    params: Promise<{ roomId: string }>
- }
-
-export default async function CanvasPage({ params }: PageProps) {
-    const {roomId} =  await params;
+export default async function CanvasPage({ params }: {
+    params: {
+        roomId: string
+    }
+}) {
+    const roomId = (await params).roomId;
 
     return <RoomCanvas roomId={roomId} />
    
 }*/
+
+import { RoomCanvas } from "@/components/RoomCanvas";
+
+interface PageProps {
+  params: { slug: string };
+}
+
+export default async function CanvasPage({ params }: PageProps) {
+  const resolvedParams = await params;
+  return <RoomCanvas roomId={resolvedParams.slug} />;
+}
