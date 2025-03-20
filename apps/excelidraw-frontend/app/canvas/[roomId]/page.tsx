113ab893ea26545509ae577c5fda3a2656b0d1e7
Rohan Vohra <rohanvohra055@gmail.com>
2025-03-20 17:13:22 +0530

    Updated page.tsx

--- apps/excelidraw-frontend/app/canvas/[roomId]/page.tsx
+++ apps/excelidraw-frontend/app/canvas/[roomId]/page.tsx
@@ -1,10 +1,6 @@
 import { RoomCanvas } from "@/components/RoomCanvas";
 
-interface PageProps {
-  params: { roomId: string }; // Type matches route parameter
-}
 
-export default function CanvasPage({ params }: PageProps) {
-  // Direct access to slug parameter
+export default function CanvasPage({ params }: { params: { roomId: string } }) {
   return <RoomCanvas roomId={params.roomId} />;
 }
